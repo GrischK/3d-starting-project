c7d0927d072cfa477be9c979ffe33d8e0dbb89d1
GrischK <grischka.gorski@laposte.net>
2024-12-04 11:15:05 +0100

    🐛 Fix leva issue for color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import { Canvas } from "@react-three/fiber";
-import { Grid } from "@react-three/drei";
+import { Grid, OrbitControls } from "@react-three/drei";
 import { useRef } from "react";
 import { useControls } from "leva";
 
 function App() {
   const ref = useRef();
 
-  const { position } = useControls({
+  const { position, color, transparent, opacity, intensity } = useControls({
     position: {
-      x: 0, y: 0, z: 0,
-    }
+      x: 0.5, y: 0.5, z: 0.5,
+    },
+    color: "#ff0000",
+    opacity: {
+      value: 0.5,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+    transparent: true,
+    intensity: {
+      value: 0.5,
+      min: 0,
+      max: 20,
+      step: 0.01,
+    },
   })
 
   return (
     <Canvas camera={{ position: [3, 3, 3], fov: 75 }}>
       {/*<axesHelper/>*/}
+      <OrbitControls />
+      <ambientLight intensity={intensity} />
       <mesh ref={ref} position={[position.x, position.y, position.z]}>
         <boxGeometry />
-        <meshNormalMaterial />
+        <meshStandardMaterial color={color} transparent={transparent} opacity={opacity} />
       </mesh>
       <Grid
         sectionSize={3}
