755016f6186f427cd5c2d553aaf0811020862122
GrischK <grischka.gorski@laposte.net>
2024-12-19 17:33:11 +0100

    ✨ Add model

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
-import { ContactShadows, Environment } from "@react-three/drei";
+import { ContactShadows, Environment, useGLTF } from "@react-three/drei";
 import { MoveableSphere } from "./MoveableSphere";
+import { Sharky } from "./Sharky.jsx";
 
 export const Experience = () => {
   return (
     <>
+      <Sharky position-y={-1.7} position-z={2}/>
       <MoveableSphere position-z={-10} scale={3} />
       <MoveableSphere />
       <MoveableSphere position-x={-2} />
