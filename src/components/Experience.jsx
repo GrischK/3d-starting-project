252af27f5f329c066250010f3c1ba2d3bb05b3ae
GrischK <grischka.gorski@laposte.net>
2024-12-20 15:06:24 +0100

    ⚡️ Let's move the shark

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,11 @@
-import { ContactShadows, Environment, useGLTF } from "@react-three/drei";
+import { ContactShadows, Environment } from "@react-three/drei";
 import { MoveableSphere } from "./MoveableSphere";
 import { Sharky } from "./Sharky.jsx";
 
 export const Experience = () => {
   return (
     <>
-      <Sharky position-y={-1.7} position-z={2}/>
+      <Sharky position-y={-1.7} position-z={2} />
       <MoveableSphere position-z={-10} scale={3} />
       <MoveableSphere />
       <MoveableSphere position-x={-2} />
@@ -13,7 +13,7 @@ export const Experience = () => {
       <ContactShadows
         rotation-x={Math.PI / 2}
         position={[0, -1.6, 0]}
-        opacity={0.42}
+        opacity={1}
         scale={40}
       />
 
